c1f88e64e1a0fd9ed7f8a2c6858e6fb93c0e30cd
nibabab <461944413@qq.com>
2020-02-24 23:18:58 +0000

    git log -1

--- 3-codegen/src/ast_compile.cpp
+++ 3-codegen/src/ast_compile.cpp
@@ -34,7 +34,83 @@ void CompileRec(
         }
     
     // TODO : handle the others
+    }else if(program->type=="If"){
+        std::string label_1 = makeName("label");
+        std::string label_2 = makeName("label");
+        std::string val_1 = makeName("val");
+        std::string zero = makeName("zero");
+        std::cout<<"const "<<zero<<" 0"<<std::endl;
+        // C
+        CompileRec(val_1, program->branches[0]);
+        // 
+        std::cout<<"beq "<<val_1<<" "<<zero<<" "<<label_1<<std::endl;
+        // X
+        CompileRec(destReg, program->branches[1]);
+        // jmp label2
+        std::cout<<"beq "<<zero<<" "<<zero<<" "<<label_2<<std::endl;
+        // :label1
+        std::cout<<":"<<label_1<<std::endl;
+        // Y
+        CompileRec(destReg, program->branches[2]);
+        // :label2
+        std::cout<<":"<<label_2<<std::endl;
+
+    }else if(program->type=="Input"){
+        std::cout << "input " << destReg << std::endl;
+
+    }else if(program->type=="Output"){
+        CompileRec(destReg, program->branches[0]);
+        std::cout << "output " << destReg << std::endl;
+
+    }else if(program->type=="LessThan"){
+        std::string val_1 = makeName("val");
+        std::string val_2 = makeName("val");
+        CompileRec(val_1, program->branches[0]);
+        CompileRec(val_2, program->branches[1]);
+        std::cout << "lt " << destReg << " " << val_1 << " " << val_2 << std::endl;
+
+    }else if(program->type=="Add"){
+        std::string val_1 = makeName("val");
+        std::string val_2 = makeName("val");
+        CompileRec(val_1, program->branches[0]);
+        CompileRec(val_2, program->branches[1]);
+        std::cout<<"add "<<destReg<<" "<<val_1<<" "<<val_2<<std::endl;
+
+    }else if(program->type=="Sub"){
+        std::string val_1 = makeName("val");
+        std::string val_2 = makeName("val");
+        CompileRec(val_1, program->branches[0]);
+        CompileRec(val_2, program->branches[1]);
+        std::cout<<"sub "<<destReg<<" "<<val_1<<" "<<val_2<<std::endl;
+
+    }else if(program->type=="Assign"){
+        CompileRec(program->value, program->branches[0]);
+        std::string zero = makeName("zero");
+        std::cout<<"const "<<zero<<" 0"<<std::endl;
+        std::cout<<"add "<<destReg<<" "<<program->value<<" "<<zero<<std::endl;
+
+    }else if(program->type=="While"){
         
+        std::string val_1 = makeName("val");
+        std::string val_2 = makeName("val");
+        std::string label_1 = makeName("label");
+        std::string label_2 = makeName("label");
+        std::string zero = makeName("zero");
+        std::cout<<"const "<<zero<<" 0"<<std::endl;
+        // :label_1
+        std::cout<<":"<<label_1<<std::endl;
+        // C
+        CompileRec(val_1, program->branches[0]);
+        // 
+        std::cout<<"beq "<<val_1<<" "<<zero<<" "<<label_2<<std::endl;
+        // X
+        CompileRec(val_2, program->branches[1]);
+        // jmp label_1
+        std::cout<<"beq "<<zero<<" "<<zero<<" "<<label_1<<std::endl;
+        // :label2
+        std::cout<<":"<<label_2<<std::endl;
+        std::cout<<"const "<<destReg<<" 0"<<std::endl;
+
     }else{
         throw std::runtime_error("Unknown construct '"+program->type+"'");
     }
--- 3-codegen/src/ast_interpret.cpp
+++ 3-codegen/src/ast_interpret.cpp
@@ -13,7 +13,9 @@ int32_t Interpret(
         return std::atol(program->type.c_str());
         
     // TODO : Check for things matching reId
-        
+    }else if(regex_match(program->type, reId)){
+        return context.bindings[program->type.c_str()];
+
     }else if(program->type=="Param"){
         unsigned index=atol(program->value.c_str());
         auto value=context.params.at(index);
@@ -23,9 +25,53 @@ int32_t Interpret(
         int32_t val=Interpret(context, program->branches.at(0));
         std::cout<<val<<std::endl;
         return val;
-        
     // TODO: Handle other constructs
-        
+    }else if(program->type=="LessThan"){
+        int32_t val_1 = Interpret(context, program->branches.at(0));
+        int32_t val_2 = Interpret(context, program->branches.at(1));
+        return val_1 < val_2 ? 1 : 0;
+
+    }else if(program->type=="Seq"){
+        unsigned last_index = program->branches.size() - 1;
+        for(unsigned i=0; i<last_index; i++) {
+            Interpret(context, program->branches.at(i));
+        }
+        return Interpret(context, program->branches.at(last_index));
+
+    }else if(program->type=="Assign"){
+        int32_t val = Interpret(context, program->branches.at(0));
+        context.bindings[program->value] = val;
+        return val;
+    }else if(program->type=="Input"){
+        int32_t val;
+        std::cin >> val;
+        return val;
+
+    }else if(program->type=="Add"){
+        int32_t val_1 = Interpret(context, program->branches.at(0));
+        int32_t val_2 = Interpret(context, program->branches.at(1));
+        return val_1 + val_2;
+    
+    }else if(program->type=="Sub"){
+        int32_t val_1 = Interpret(context, program->branches.at(0));
+        int32_t val_2 = Interpret(context, program->branches.at(1));
+        return val_1 - val_2;
+    
+    }else if(program->type=="If"){
+        int32_t val_1 = Interpret(context, program->branches.at(0));
+        int32_t val_2;
+        if (val_1){
+            val_2 = Interpret(context, program->branches.at(1));
+        } else {
+            val_2 = Interpret(context, program->branches.at(2));
+        }
+        return val_2;
+     }else if(program->type=="While"){
+        while (Interpret(context, program->branches.at(0))) {
+            Interpret(context, program->branches.at(1));
+        }
+        return 0;
+
     }else{
         throw std::runtime_error("Unknown construct '"+program->type+"'");
     }
